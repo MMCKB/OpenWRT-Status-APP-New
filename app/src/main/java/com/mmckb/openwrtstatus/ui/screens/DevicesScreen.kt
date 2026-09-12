9698713f06f2901aa0cc8ab3276c81dd81b5e3a3
MMCKB <1979161564@qq.com>
2026-09-13 00:39:00 +0800

    修复：补 AppShapes 导入（theme 包）

--- app/src/main/java/com/mmckb/openwrtstatus/ui/screens/DevicesScreen.kt
+++ app/src/main/java/com/mmckb/openwrtstatus/ui/screens/DevicesScreen.kt
@@ -56,6 +56,7 @@ import com.mmckb.openwrtstatus.ui.components.AppDialog
 import com.mmckb.openwrtstatus.ui.components.CardSectionTitle
 import com.mmckb.openwrtstatus.ui.components.PredictiveBackEasing
 import com.mmckb.openwrtstatus.ui.components.rememberTopBarPadding
+import com.mmckb.openwrtstatus.ui.theme.AppShapes
 import com.mmckb.openwrtstatus.ui.theme.LocalAppColors
 
 /**
