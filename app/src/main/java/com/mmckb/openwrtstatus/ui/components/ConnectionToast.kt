24ed6401ef2ddb21764a9ac51526f0c0e32a6214
MMCKB <MMCKB@users.noreply.github.com>
2026-09-19 16:54:58 +0800

    修复：补 Icon 导入

--- app/src/main/java/com/mmckb/openwrtstatus/ui/components/ConnectionToast.kt
+++ app/src/main/java/com/mmckb/openwrtstatus/ui/components/ConnectionToast.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Router
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
